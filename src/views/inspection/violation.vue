659303d5563ba8973838037fd95f1d1c6809004a
15812281117 <2641345423@qq.com>
2021-12-19 23:07:09 +0800

    新增页面

---
+++ src/views/inspection/violation.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      abc
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+      condition:true
+    }
+  },
+  methods:{
+   
+  },
+  created(){
+  
+  },
+  components:{
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
