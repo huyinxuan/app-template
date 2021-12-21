066987ee2a253dc46c32c1bcb8289a4c3b0a976a
wlyaddress <13380314014@163.com>
2021-12-21 18:05:30 +0800

    巡查上报接口对接

--- src/views/sh/xcsb.vue
+++ src/views/sh/xcsb.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <div>
-      <van-nav-bar :title="title" left-arrow border fixed placeholder  @click-left="onClickLeft"/>
+      <van-nav-bar :title="title" left-arrow border fixed placeholder   @click-left="onClickLeft"/>
       
     </div>
     <!-- 输入任意文本 -->
     <van-form @submit="onSubmit">
       <van-field v-model="text" label="上传人员：" />
-      <!-- <van-field v-model="text" label="事件类型：" /> -->
-      <van-cell is-link title="事件类型" @click="show2 = true" />
-      <van-action-sheet v-model="show2" :actions="actions" @select="onSelect" />     
+      <van-cell is-link title="事件类型"  @click="show2 = true" />
+      <van-action-sheet v-model="punishId" :actions="actionsj" @select="sjSelect" />     
+
       <van-cell is-link @click="openMap" :value="point.address" title="违规地点：" />
-      <van-field v-model="text" label="属地街道：" />
+
+       <van-cell is-link title="属地街道："  @click="show1 = true" />
+      <van-action-sheet v-model="regionId" :actions="actions" @select="onSelect" /> 
+      
       <van-field v-model="text" label="运输单位：" />
 
       <van-field name="uploader" label="照片：">
@@ -33,19 +36,21 @@
 </template>
 
 <script>
+import { regionListAll } from "@/api/payMG";
+import { punishDailyDropList } from "@/api/userMG";
 import selectMap from '@/components/map'
 export default {
   data() {
     return {
-      val:'123',
-      date: '',
-      date1:'',
+     // val:'123',
+      // date: '',
+      // date1:'',
       show2: false,
       show1: false,
-      show: false,
       title:"巡查上报",
       uploader: [],
-      actions: [{ name: '选项一' }, { name: '选项二' }, { name: '选项三' }],
+      actionsj: [],
+      actions: [],
       showMap:false,
       point:{
         lat:-1,
@@ -53,44 +58,55 @@ export default {
         address:'',
         showMap:false
       }
-
     };
   },
   components:{
     selectMap
   },
+  created(){
+    this.sjSelect(),
+     this.sdSelect()
+  },
   methods: {
+     // 事件类型下拉查询
+     sjSelect(){
+      punishDailyDropList().then(res => {
+            if (res.code == 200) {
+              this.actionsj = res.data;
+            } else {
+              this.$message.error(res.msg);
+            }
+      });
+     },
     // 事件类型下拉
     onSelect(item) {
       // 默认情况下点击选项时不会自动收起
       // 可以通过 close-on-click-action 属性开启自动收起
-      this.show = false;
-      Toast(item.name);
+      this.show2 = false;
+      Toast(item.reason);
     },
-    onLoad() {
-      // 异步更新数据
-      
+    // 属地街道下拉查询
+     sdSelect(){
+      regionListAll().then(res => {
+            if (res.code == 200) {
+              this.actions = res.data;
+            } else {
+              this.$message.error(res.msg);
+            }
+      });
+     },
+    // 属地街道下拉
+    onSelect(item) {
+      this.show1 = false;
+      Toast(item.name);
     },
     //打开地图选择窗口
     openMap(){
-     
       this.point.showMap=true;
       console.log(this.point);
     },
     onClickLeft(){
       this.$router.go(-1)
-    },
-      formatDate(date) {
-      return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-    },
-    onConfirm(date) {
-      this.show = false;
-      this.date = this.formatDate(date);
-    },
-    onConfirm1(date1){
-      const [start, end] = date1;
-      this.show1 = false;
-      this.date1 = `${this.formatDate(start)} - ${this.formatDate(end)}`;
     },
     onSubmit(values){
       console.log('submit', values);
