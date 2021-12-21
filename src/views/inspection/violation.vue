4ab3dbc016ddff42c79b344b3d2ffacf718af901
15812281117 <2641345423@qq.com>
2021-12-21 22:22:06 +0800

    提交

--- src/views/inspection/violation.vue
+++ src/views/inspection/violation.vue
@@ -1,116 +1,166 @@
 <template>
-  <div  class="home">
-        <van-nav-bar
-            title="违法处置"
-            left-arrow
-            border
-            fixed
-            placeholder 
-            @click-left="onClickLeft"
-          />
-      <van-loading v-if="condition" size="24px">加载中...</van-loading>
-      <template v-else>
-          <van-search
-            v-model="search"
-            shape="round"
-            background="#f2f2f2"
-            placeholder="请输入搜索关键词"
-          />
-        <van-tabs v-model="active" animated>
-          <van-tab title="待处理">
-              <div class="van-box" v-for="(item, index) in 10" :key="index">
-                  <van-row class="card_title">
-                        <van-col span="8">北二路路口</van-col>
-                        <van-col offset="13" class="card_status_0">待处理</van-col>
-                    </van-row>
-                    <van-row>
-                        <van-col span="12">违规日期：2019-04-01</van-col>
-                        <van-col span="12">违规类型：道路遗撒</van-col>
-                    </van-row>
-                    <van-row>
-                        <van-col span="12">违规来源：智能抓拍</van-col>
-                        <van-col span="12">处理结果：暂无</van-col>
-                    </van-row>
-              </div>
-          </van-tab>
-          <van-tab title="已处理">
-                <div class="van-box" v-for="(item, index) in 10" :key="index">
-                  <van-row class="card_title">
-                        <van-col span="8">北二路路口</van-col>
-                        <van-col offset="13" class="card_status_1">已处理</van-col>
-                    </van-row>
-                    <van-row>
-                        <van-col span="12">违规日期：2019-04-01</van-col>
-                        <van-col span="12">违规类型：道路遗撒</van-col>
-                    </van-row>
-                    <van-row>
-                        <van-col span="12">违规来源：智能抓拍</van-col>
-                        <van-col span="12">处理结果：暂无</van-col>
-                    </van-row>
-                </div>
-          </van-tab>
-        </van-tabs>
-      </template>
+  <div class="home">
+    <van-nav-bar
+      title="违法处置"
+      left-arrow
+      border
+      fixed
+      placeholder
+      @click-left="onClickLeft"
+    />
+    <van-loading v-if="condition" size="24px">加载中...</van-loading>
+    <template v-else>
+      <van-search
+        v-model="searchForm.model"
+        shape="round"
+        background="#f2f2f2"
+        placeholder="请输入搜索关键词"
+      />
+      <van-tabs v-model="active" animated>
+        <van-tab title="待处理">
+          <div class="van-box" v-for="(item, index) in tableData" :key="index">
+            <van-row class="card_title">
+              <van-col span="8" ><div class="van-ellipsis">{{ item.regionName }}</div></van-col>
+              <van-col
+                offset="13"
+                class="card_status_0"
+                v-show="item.status == 1"
+                >待处理</van-col
+              >
+              <van-col
+                offset="13"
+                class="card_status_0"
+                v-show="item.status == 2"
+                >已超期</van-col
+              >
+              <van-col
+                offset="13"
+                class="card_status_0"
+                v-show="item.status == 3"
+                >待审核</van-col
+              >
+              <van-col
+                offset="13"
+                class="card_status_1"
+                v-show="item.status == 4"
+                >已处理</van-col
+              >
+            </van-row>
+            <van-row>
+              <van-col span="12" ><div class="van-ellipsis">违规日期：{{item.createTime}}</div></van-col>
+              <van-col span="12" ><div class="van-ellipsis">违规类型：{{item.punishName}}</div></van-col>
+            </van-row>
+            <van-row>
+              <van-col span="12">违规来源：<template v-show="item.sourceType==1">巡查</template><template v-show="item.sourceType==2">群众举报</template><template v-show="item.sourceType==3">智能抓拍</template></van-col>
+              <van-col span="12" ><div class="van-ellipsis">处理结果：{{item.handleResult}}</div></van-col>
+            </van-row>
+          </div>
+        </van-tab>
+      </van-tabs>
+    </template>
   </div>
 </template>
 
 <script>
-import dataStatis from '@/components/dataStatis'
+import { ComplaintsList } from "@/api/inspectionapi";
+
+import dataStatis from "@/components/dataStatis";
 export default {
   data() {
     return {
       active: 0,
-      condition:true
-    }
+      condition: true,
+      tableData: [],
+      //查询变量
+      searchForm: {
+        pageNum: 1,
+        pageSize: 10,
+        projectName: "",
+      },
+    };
   },
-  methods:{
-    onClickLeft(){
-      this.$router.go(-1)
-    }
+  watch: {},
+
+  methods: {
+    onClickLeft() {
+      this.$router.go(-1);
+    },
+
+    //重置
+    reset() {
+      this.searchForm.projectName = "";
+      this.DataList(this.searchForm);
+    },
+
+    //查询
+    search() {
+      this.DataList(this.searchForm);
+    },
+
+    //获取数据
+    DataList(searchForm) {
+      var than = this;
+      console.log("searchForm:", searchForm);
+      than.loadingBat = true;
+      ComplaintsList(searchForm).then((res) => {
+        than.loadingBat = false;
+        if (res.code !== 200) {
+          this.$message({
+            type: "info",
+            message: res.msg,
+          });
+        } else {
+          than.condition = false;
+          console.log("返回参数res:", res.data);
+          than.tableData = res.data.list;
+        
+        }
+      });
+    },
   },
-  created(){
-    setTimeout(() => {
-      this.condition = false
-    }, 650);
+  created() {
+     this.search();
+
+    
   },
-  components:{
-    dataStatis
-  }
-}
+  components: {
+    dataStatis,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.home{
+.home {
   background-color: #f7f7f7;
 }
-.van-loading{
+.van-loading {
   text-align: center;
   margin-top: 220px;
 }
-.van-box{
-  color:#999999;
-  background-color: #FFf;
+.van-box {
+  color: #999999;
+  background-color: #fff;
   width: 710px;
-  margin:36px auto 40px auto;
+  margin: 36px auto 40px auto;
   border-radius: 20px;
   padding: 40px;
   box-shadow: 0px 0px 10px rgb(0 0 0 / 5%);
   line-height: 50px;
 }
-.van-search__content{
-    background-color: #fff !important;
+.van-search__content {
+  background-color: #fff !important;
 }
-.van-box .card_title .van-col:nth-child(1){
+.van-box .card_title .van-col:nth-child(1) {
   margin-bottom: 15px;
   font-weight: bold;
   font-size: 32px;
 }
-.card_status_0{ 
-  color: #F59A23;
-  font-size:20px
+.card_status_0 {
+  color: #f59a23;
+  font-size: 20px;
 }
-.card_status_1{
-  color: #70B603;
-  font-size:20px
+.card_status_1 {
+  color: #70b603;
+  font-size: 20px;
 }
 </style>
