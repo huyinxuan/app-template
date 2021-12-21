5bb6ecb6a4e1bd2c190a325a6ec88e9a7b83fae7
15812281117 <2641345423@qq.com>
2021-12-21 22:22:55 +0800

    提交

--- src/router/inspection.js
+++ src/router/inspection.js
@@ -1,6 +1,10 @@
 
-//违法处置详情（完成）
+//违法处置详情
 import violationDetails from '@/views/inspection/violationDetails';
+
+//违法处置
+import violation from '@/views/inspection/violation';
+
 //历史记录
 import history from '@/views/inspection/history';
 //历史详情记录
@@ -29,6 +33,16 @@ export default [
         path: '/historyDetails',
         name: '历史详情',
         component: historyDetails,
+        meta: {
+            index:34,
+            requireAuth: true
+        }
+    }
+    ,
+    {
+        path: '/violation',
+        name: '违法处置',
+        component: violation,
         meta: {
             index:34,
             requireAuth: true
