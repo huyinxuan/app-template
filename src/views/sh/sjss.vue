448b04f1987b386282caea39318e2b111cd10447
wlyaddress <13380314014@163.com>
2021-12-21 13:40:09 +0800

    提交静态界面

---
+++ src/views/sh/sjss.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div>
+      <van-nav-bar  :title="title" left-arrow border fixed placeholder right-text="提交" @click-right="add()"  @click-left="onClickLeft"/>
+       <!-- <van-nav-bar   border fixed placeholder  @click-left="onClickLeft"/> -->
+    </div>
+    <!-- 输入任意文本 -->
+    <van-form @submit="onSubmit">
+      <van-field
+          v-model="message"
+          rows="3"
+          autosize
+          label="申诉理由"
+          type="textarea"
+          placeholder="请输入问题说明"
+          show-word-limit
+        />
+    </van-form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+     date: '',
+     title:"事件申诉",
+    };
+  },
+
+  methods: {
+    add(){
+      console.log(1111111)
+    },
+    // 返回上一页
+    onClickLeft(){
+      this.$router.go(-1)
+    },
+    // 提交
+    onSubmit(values){
+      console.log('submit', values);
+    }
+  },
+  components:{
+    
+  }
+};
+
+</script>
+
+<style>
+.van-col{
+  font-size: 26px;
+}
+.van-col--24{
+  font-size: 36px;
+  font-weight: 500;
+}
+
+</style>
---
+++ src/views/sh/sptjdataStatis.vue
@@ -0,0 +1,61 @@
+<template>
+  <div  class="home">
+        <van-nav-bar
+            title="数据统计"
+            left-arrow
+            border
+            fixed
+            placeholder 
+            @click-left="onClickLeft"
+          />
+      <van-loading v-if="condition" size="24px">加载中...</van-loading>
+      <template v-else>
+        <van-tabs v-model="active" animated>
+          <van-tab title="月">
+            <dataStatis :tabindex="active"></dataStatis>
+          </van-tab>
+          <van-tab title="季">
+            <dataStatis :tabindex="active"></dataStatis>
+          </van-tab>
+          <van-tab title="年">
+            <dataStatis :tabindex="active"></dataStatis>
+          </van-tab>
+        </van-tabs>
+      </template>
+  </div>
+</template>
+
+<script>
+import dataStatis from '@/views/sh/dataStatis'
+export default {
+  data() {
+    return {
+      active: 0,
+      condition:true
+    }
+  },
+  methods:{
+    onClickLeft(){
+      this.$router.go(-1)
+    }
+  },
+  created(){
+    setTimeout(() => {
+      this.condition = false
+    }, 650);
+  },
+  components:{
+    dataStatis
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home{
+  background-color: #f7f7f7;
+}
+.van-loading{
+  text-align: center;
+  margin-top: 220px;
+}
+</style>
