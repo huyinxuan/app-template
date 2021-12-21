2b2ed724726cef5f61b25ac6cc80445c9d871a89
huyinxuan <492326552@qq.com>
2021-12-21 12:57:12 +0800

    核准制样式优化

--- src/views/sh/hezhun.vue
+++ src/views/sh/hezhun.vue
@@ -17,51 +17,48 @@
             placeholder="请输入搜索关键词"
           />
           <div class="home-canvan">
-        <div v-for="item in list" :key="item" class="hezhun-cont">
+          <div v-for="item in list" :key="item" class="hezhun-cont">
             <van-row>
                <van-col span="24" ><span>东区审批建准字[2021]第12号</span></van-col>
             </van-row>
             <van-row >
-                <van-col span="8"><span>工程名称：</span></van-col>
-                <van-col span="16"><span>{{item.name}}</span></van-col>
+                <van-col span="23"><span>工程名称：{{item.name}}</span></van-col>
             </van-row>
             <van-row >
-                <van-col span="8"><span>工程地址：</span></van-col>
-                <van-col span="16"><span>{{item.name}}</span></van-col>
+                <van-col span="23"><span>工程地址：{{item.name}}</span></van-col>
             </van-row>
             <van-row >
-                <van-col span="8"><span>建设单位：</span></van-col>
-                <van-col span="16"><span>{{item.name}}</span></van-col>
+                <van-col span="23"><span>建设单位：{{item.name}}</span></van-col>
             </van-row>
             <van-row >
-                <van-col span="8"><span>施工单位：</span></van-col>
-                <van-col span="16"><span>{{item.name}}</span></van-col>
+                <van-col span="23"><span>施工单位：{{item.name}}</span></van-col>
             </van-row>
             <van-row >
-                <van-col span="8"><span>运输单位：</span></van-col>
-                <van-col span="16"><span>{{item.name}}</span></van-col>
+                <van-col span="23"><span>运输单位：{{item.name}}</span></van-col>
             </van-row>
             <van-row >
-                <van-col span="8"><span>处置数量及方式：</span></van-col>
-                <van-col span="16"><span>{{item.name}}</span></van-col>
+                <van-col span="23"><span>处置数量及方式：{{item.name}}</span></van-col>
+            </van-row>
+            <van-row >z
+                <van-col span="23"><span>处置场所：{{item.name}}</span></van-col>
             </van-row>
             <van-row >
-                <van-col span="8"><span>处置场所：</span></van-col>
-                <van-col span="16"><span>{{item.name}}</span></van-col>
+                <van-col span="23"><span>证件有效期：{{item.name}}</span></van-col>
             </van-row>
             <van-row >
-                <van-col span="8"><span>证件有效期：</span></van-col>
-                <van-col span="16"><span>{{item.name}}</span></van-col>
+                <van-col span="23"><span>办理日期：{{item.name}}</span></van-col>
             </van-row>
-            <van-row >
-                <van-col span="8"><span>办理日期：</span></van-col>
-                <van-col span="16"><span>{{item.name}}</span></van-col>
+            <van-row class="btns">
+                <van-col span="8">
+                  <van-button plain type="info" icon="delete">删除</van-button>
+                </van-col>
+                <van-col span="8">
+                  <van-button plain type="info" icon="edit">编辑</van-button>
+                </van-col>
+                <van-col span="8">
+                  <van-button plain type="info" icon="photo-o">照片</van-button>
+                </van-col>
             </van-row>
-             <div>
-                <van-button plain type="info" icon="delete">删除</van-button>
-                <van-button plain type="info" icon="edit">编辑</van-button>
-                <van-button plain type="info" icon="photo-o">照片</van-button>
-              </div>
           </div>
           </div>
           <van-tabbar v-model="active" placeholder fixed>
@@ -115,11 +112,19 @@ export default {
   margin-top: 220px;
 }
 .van-col{
-  font-size: 26px;
+  font-size: 24px;
+  color: #999999;
+  font-weight: 400;
 }
 .van-col--24{
-  font-size: 36px;
-  font-weight: 500;
+  font-family: '微软雅黑 Bold', '微软雅黑 Regular', '微软雅黑', sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  color: #999999;
+  text-align: left;
+  line-height: 24px;
+  font-size: 28px;
+  margin-bottom: 25px;
 }
 .home{
   background-color: #f7f7f7;
@@ -135,10 +140,30 @@ export default {
   margin-bottom: 40px;
   border-radius: 20px;
   padding: 40px;
+  padding-bottom: 0px;
+  line-height: 47px;
+  font-family: '微软雅黑', sans-serif;
+  font-weight: 300;
+  font-style: normal;
+  font-size: 12px;
+  color: #999999;
+  text-align: left;
 }
 .van-box{
   .font-sz{
     font-size: 28px;
+  }
+}
+.btns{
+  margin-top:20px;
+  
+  .van-col{
+    text-align: center;
+  }
+  button{
+    border: 0;
+    position: relative;
+    color: #CCC;
   }
 }
 </style>
