bddbf7315ce1275d0e0317744e823aa8ea7f20f8
huyinxuan <492326552@qq.com>
2021-12-21 12:38:19 +0800

    优化事件查询css

--- src/views/other/weigui.vue
+++ src/views/other/weigui.vue
@@ -18,11 +18,10 @@
           />
         <van-tabs v-model="active" animated>
           <van-tab title="待处理">
-              <div class="van-box" v-for="(item, index) in 3" :key="index">
-                  <van-row>
+              <div class="van-box" v-for="(item, index) in 10" :key="index">
+                  <van-row class="card_title">
                         <van-col span="8">北二路路口</van-col>
-                        <van-col span="8"></van-col>
-                        <van-col span="8">待处理</van-col>
+                        <van-col offset="13" class="card_status_0">待处理</van-col>
                     </van-row>
                     <van-row>
                         <van-col span="12">违规日期：2019-04-01</van-col>
@@ -35,7 +34,20 @@
               </div>
           </van-tab>
           <van-tab title="已处理">
-              <div class="van-box"></div>
+                <div class="van-box" v-for="(item, index) in 10" :key="index">
+                  <van-row class="card_title">
+                        <van-col span="8">北二路路口</van-col>
+                        <van-col offset="13" class="card_status_1">已处理</van-col>
+                    </van-row>
+                    <van-row>
+                        <van-col span="12">违规日期：2019-04-01</van-col>
+                        <van-col span="12">违规类型：道路遗撒</van-col>
+                    </van-row>
+                    <van-row>
+                        <van-col span="12">违规来源：智能抓拍</van-col>
+                        <van-col span="12">处理结果：暂无</van-col>
+                    </van-row>
+                </div>
           </van-tab>
         </van-tabs>
       </template>
@@ -76,14 +88,29 @@ export default {
   margin-top: 220px;
 }
 .van-box{
-    background-color: #FFf;
-    width: 710px;
-    margin:36px auto 40px auto;
-    border-radius: 20px;
-    padding: 40px;
-    box-shadow: 0px 0px 10px rgb(0 0 0 / 5%);
+  color:#999999;
+  background-color: #FFf;
+  width: 710px;
+  margin:36px auto 40px auto;
+  border-radius: 20px;
+  padding: 40px;
+  box-shadow: 0px 0px 10px rgb(0 0 0 / 5%);
+  line-height: 50px;
 }
 .van-search__content{
     background-color: #fff !important;
+}
+.van-box .card_title .van-col:nth-child(1){
+  margin-bottom: 15px;
+  font-weight: bold;
+  font-size: 32px;
+}
+.card_status_0{ 
+  color: #F59A23;
+  font-size:20px
+}
+.card_status_1{
+  color: #70B603;
+  font-size:20px
 }
 </style>
