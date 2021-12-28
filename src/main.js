679292e95d724ded90d4ebf6af409bdcd116c1b6
hyx <1364892269@qq.com>
2021-12-28 20:40:42 +0800

    x

--- src/main.js
+++ src/main.js
@@ -52,13 +52,13 @@ import { ImagePreview } from 'vant';
 Vue.use(ImagePreview);
 
 // Vue.use(plugins).use(components);
-vue.prototype.$setCookie = function (cname,cvalue,exdays){
+Vue.prototype.$setCookie = function (cname,cvalue,exdays){
 	var d = new Date();
 	d.setTime(d.getTime()+(exdays*24*60*60*1000));
 	var expires = "expires="+d.toGMTString();
 	document.cookie = cname+"="+cvalue+"; "+expires;
 }
-vue.prototype.$getCookie = function(cname){
+Vue.prototype.$getCookie = function(cname){
 	var name = cname + "=";
 	var ca = document.cookie.split(';');
 	for(var i=0; i<ca.length; i++) {
